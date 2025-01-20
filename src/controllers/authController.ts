0105b2512ac5a8ca956e7de4aada95ee962349e0
Muhammad <jamiludeensani@gmail.com>
2025-01-20 16:30:59 +0100

    Add user profile retrieval and deletion functionality

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -77,13 +77,25 @@ export const getUserProfile = async (req: Request, res: Response): Promise<void>
       res.status(500).json({ message: 'Server error', error: errorMessage });
     }
   };
+
+// Get Users Profile (GET /profile)
+  export const getUsersProfile = async (req: Request, res: Response): Promise<void> => {
+    try {
+      const users = await User.find();
+      res.status(200).json(users);
+    } catch (err) {
+      const errorMessage = err instanceof Error ? err.message : 'Unknown error';
+      res.status(500).json({ message: 'Server error', error: errorMessage });
+    }
+  };
   
   // Update User Profile (PUT /profile)
   export const updateUserProfile = async (req: Request, res: Response): Promise<void> => {
+    const {id} = req.params;
     const { profilePicture, skills, availability }: { profilePicture?: string, skills?: string, availability?: boolean } = req.body;
   
     try {
-      const user = await User.findById((req as any).user.id); // From the authenticate middleware
+      const user = await User.findById(id); // From the authenticate middleware
       if (!user) {
         res.status(404).json({ message: 'User not found' });
         return;
@@ -95,6 +107,24 @@ export const getUserProfile = async (req: Request, res: Response): Promise<void>
   
       await user.save();
       res.status(200).json(user);
+    } catch (err) {
+      const errorMessage = err instanceof Error ? err.message : 'Unknown error';
+      res.status(500).json({ message: 'Server error', error: errorMessage });
+    }
+  };
+
+  // Delete User Profile (DELETE /profile)
+  export const deleteUser = async (req: Request, res: Response): Promise<void> => {
+    const {id} = req.params;
+    try {
+      const user = await User.findById(id); // From the authenticate middleware
+      if (!user) {
+        res.status(404).json({ message: 'User not found' });
+        return;
+      }
+  
+      await user.deleteOne();
+      res.status(200).json({ message: 'User deleted successfully' });
     } catch (err) {
       const errorMessage = err instanceof Error ? err.message : 'Unknown error';
       res.status(500).json({ message: 'Server error', error: errorMessage });
