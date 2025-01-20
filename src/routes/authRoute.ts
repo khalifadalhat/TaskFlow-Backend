d08942bc65a970c0d6e6511061b6a8cc7fe00474
Muhammad <jamiludeensani@gmail.com>
2025-01-20 16:32:54 +0100

    Remove commented JWT token from authRoute

--- src/routes/authRoute.ts
+++ src/routes/authRoute.ts
@@ -13,6 +13,4 @@ router.post('/register', async (req: Request, res: Response, next: NextFunction)
     await loginUser(req, res).catch(next);
   });
 
-  // eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY3OGQzMzE2MzVkYzkyNGFlZWEyYWE0ZSIsInJvbGUiOiJBZG1pbiIsImlhdCI6MTczNzMwNjk0MiwiZXhwIjoxNzM3MzEwNTQyfQ.Ul4ex9NK6CI74iXI6551EgSudSUauxIViJM64YJkB1g
-
 export default router;
