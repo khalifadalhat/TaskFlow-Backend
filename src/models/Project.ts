0894e3cf0f14f45a91ce8ad4031d050697400934
Muhammad <jamiludeensani@gmail.com>
2025-01-23 14:21:25 +0100

    Refactor projectMembers field to use ObjectId references for improved data integrity

--- src/models/Project.ts
+++ src/models/Project.ts
@@ -1,11 +1,11 @@
-import { Schema, model, Document } from "mongoose";
+import mongoose, { Schema, model, Document } from "mongoose";
 
 interface IProject extends Document {
   projectName: string;
   projectDescription: string;
   projectMilestone: string;
   projectTasks: string[];
-  projectMembers: string[];
+  projectMembers: mongoose.Schema.Types.ObjectId[];
   projectStartDate: Date;
   projectEndDate: Date;
 }
@@ -15,7 +15,10 @@ const projectSchema = new Schema<IProject>({
     projectDescription: { type: String, required: true },
     projectTasks: { type: [String], default: [] },
     projectMilestone: { type: String, required: true },
-    projectMembers: { type: [String], default: [] },
+    projectMembers: [{ 
+      type: mongoose.Schema.Types.ObjectId, 
+      ref: 'User' 
+    }],
     projectStartDate: { type: Date, required: true },
     projectEndDate: { type: Date, required: true },
     
